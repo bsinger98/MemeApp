e42f78170820adfc175311ebbfce468e1f13c148
mschwimmer <MattSchwimmer@Matthews-MBP.home>
2016-12-18 21:47:28 -0500

    Added green and blue boxes
    and the thing is crashing when i hit login

--- MemeApp/memeSwipeController.swift
+++ MemeApp/memeSwipeController.swift
@@ -11,6 +11,17 @@ import UIKit
 class memeSwipeController: UIViewController {
     
     @IBOutlet weak var memeImg: UIImageView!
+    @IBOutlet weak var greenSquare: UIView!
+    @IBOutlet weak var blueSquare: UIView!
+    
+    private var originalBounds = CGRect.zero
+    private var originalCenter = CGPoint.zero
+    
+    
+    private var animator: UIDynamicAnimator!
+    private var attachmentBehavior: UIAttachmentBehavior!
+    private var pushBehavior: UIPushBehavior!
+    private var itemBehavior: UIDynamicItemBehavior!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +41,24 @@ class memeSwipeController: UIViewController {
     @IBAction func swipeLeft(_ swipe:UISwipeGestureRecognizer) {
         print("Swipe Left")
         
+    }
+    
+    @IBAction func handleAttachmentGesture(sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: self.view)
+        let boxLocation = sender.location(in: self.memeImg)
+        
+        switch sender.state {
+        case .began:
+            print("Your touch start position is \(location)")
+            print("Start location in image is \(boxLocation)")
+            
+        case .ended:
+            print("Your touch end position is \(location)")
+            print("End location in image is \(boxLocation)")
+            
+        default:
+            break
+        }
     }
 
     
