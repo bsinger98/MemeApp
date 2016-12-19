cfc18a38c61c512956d0272929b1d834ac486a94
Brian Singer <brian@convolve.com>
2016-12-18 21:17:51 -0500

    img outlet

--- MemeApp/memeSwipeController.swift
+++ MemeApp/memeSwipeController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class memeSwipeController: UIViewController {
     
+    @IBOutlet weak var memeImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
