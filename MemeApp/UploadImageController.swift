7ed3583d4ad27617394627b989e8f4dd11524134
Brian Singer <brian@convolve.com>
2016-12-20 13:44:50 -0500

    Shitty adding image feature, needs to look better

---
+++ MemeApp/UploadImageController.swift
@@ -0,0 +1,59 @@
+//
+//  UploadImageController.swift
+//  MemeApp
+//
+//  Created by Brian Singer on 12/17/16.
+//  Copyright © 2016 Brian Singer. All rights reserved.
+//
+
+import UIKit
+
+class UploadImageController: UIViewController, UIImagePickerControllerDelegate,  UINavigationControllerDelegate {
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var memeSelectImg: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    
+    @IBAction func uploadImage(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func selectImage(_ sender: Any) {
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set image picker to use photolibrary and set delegate to controller
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self;
+        
+        // Initially disable upload button until user fills out all forms
+        uploadButton.isEnabled = false;
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Image Picker Callback
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Cast info as an image and try to put image in the selected image picture
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            memeSelectImg.contentMode = .scaleAspectFit
+            memeSelectImg.image = pickedImage
+            
+            // Allow user to upload
+            uploadButton.isEnabled = true;
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
